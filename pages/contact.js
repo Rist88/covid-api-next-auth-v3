65bf421ddbfe81bdae1cff7fc12701f0795c1581
Andrew Rudenko <andrew.rudenko9@gmail.com>
2022-06-20 18:58:19 +0300

    add initial content

---
+++ pages/contact.js
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import AppContext from "../AppContext";
+import { Container } from "react-bootstrap";
+import Navigation from "../components/Navigation";
+
+export default function About() {
+  const value = useContext(AppContext);
+  let { emailLabel, phoneLabel } = value.state.languages;
+
+  return (
+    <>
+      <Navigation />
+      <Container className="page">
+        <h2>{value.state.languages.contactTitle}</h2>
+        <div>
+          <li>{`${emailLabel}: hi@example.com`}</li>
+          <li>{`${phoneLabel}: +010112345`}</li>
+        </div>
+      </Container>
+    </>
+  );
+}

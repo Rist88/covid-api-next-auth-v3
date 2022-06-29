f798bd5175b002b2c1dbd6c0909b1841b87ae54d
Andrew Rudenko <andrew.rudenko9@gmail.com>
2022-06-29 13:57:37 +0300

    add initial next-auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 import { useState } from "react";
 import AppContext from "../AppContext";
 import languagesObject from "../languageObject";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const [languageSelected, setLanguageSelected] = useState("en");
   const languageObject = languagesObject;
   return (
-    <AppContext.Provider
-      value={{
-        state: {
-          languages: languageObject[languageSelected],
-          languageSelected: languageSelected,
-        },
-        setLanguageSelected: setLanguageSelected,
-      }}
-    >
-      <Component {...pageProps} />;
-    </AppContext.Provider>
+    <SessionProvider session={session}>
+      <AppContext.Provider
+        value={{
+          state: {
+            languages: languageObject[languageSelected],
+            languageSelected: languageSelected,
+          },
+          setLanguageSelected: setLanguageSelected,
+        }}
+      >
+        <Component {...pageProps} />;
+      </AppContext.Provider>
+    </SessionProvider>
   );
 }
 

8c96b27163c82bed8438c729b2180447561f5249
Andrew Rudenko <andrew.rudenko9@gmail.com>
2022-06-29 19:35:57 +0300

    add sign button in navbar

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,9 +1,13 @@
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useContext } from "react";
 import AppContext from "../AppContext";
 
 export default function Navigation() {
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
+
   const value = useContext(AppContext);
   let { languageSelected } = value.state;
   let { navbarAboutLink, navbarContactLink } = value.state.languages;
@@ -18,6 +22,44 @@ export default function Navigation() {
         <Navbar.Collapse id="responseve-navbar-nav">
           <Nav className="me-auto"></Nav>
           <Nav>
+            {!session && (
+              <>
+                <span className="nav-link">You are not signed in</span>
+                <Link
+                  href={`/api/auth/signin`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    signIn();
+                  }}
+                >
+                  <a className="nav-link">Sign in</a>
+                </Link>
+              </>
+            )}
+            {session?.user && (
+              <>
+                {/* {session.user.image && (
+                  <span
+                    // style={{ backgroundImage: `url('${session.user.image}')` }}
+                    className={styles.avatar}
+                  />
+                )} */}
+                <span>
+                  <small>Signed in as</small>
+                  <br />
+                  <strong>{session.user.email ?? session.user.name}</strong>
+                </span>
+                <Link
+                  href={`/api/auth/signout`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    signOut();
+                  }}
+                >
+                  <a className="nav-link">Sign out</a>
+                </Link>
+              </>
+            )}
             <Link href="/about">
               <a className="nav-link">{navbarAboutLink}</a>
             </Link>
